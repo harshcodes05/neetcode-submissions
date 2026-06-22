725fb9b774e4944e3870b52e38e48fac173be4a0
harshcodes05 <harshcodes05@gmail.com>
2026-06-22 13:43:58 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxH;
+        for(int i : stones) maxH.push(i);
+        while(maxH.size()>1) {
+            int x = maxH.top();
+            maxH.pop();
+            int y = maxH.top();
+            maxH.pop();
+            if(x==y) {
+                if(maxH.size()==0) maxH.push(0);
+                continue;
+            }
+            maxH.push(x-y);
+        }
+        return maxH.top();
+    }
+};
